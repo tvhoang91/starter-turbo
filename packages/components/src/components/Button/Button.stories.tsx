39b1b7a43fc2a99f16df705cf73c3a0b486c5bb4
Hoang Truong <tvhoang91@gmail.com>
2023-08-23 16:15:04 +0700

    add autodoc

--- packages/components/src/components/Button/Button.stories.tsx
+++ packages/components/src/components/Button/Button.stories.tsx
@@ -4,6 +4,7 @@ import Button from './Button';
 const meta: Meta<typeof Button> = {
   title: 'Button',
   component: Button,
+  tags: ['autodocs'],
   argTypes: {
     variant: {
       description: 'The type of button',
